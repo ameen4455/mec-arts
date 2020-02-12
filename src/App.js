885679fca2a101799c669f67f18b9af4a72b9d7c
ameen4455 <ameen4455@gmail.com>
2020-02-13 01:45:30 +0530

    arts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import React from 'react';
 import logo from './logo.svg';
+import Thandava from "./logo/thandava.png"
+import Rakshasa from "./logo/rakshasa.png"
+import Druva from "./logo/druva.png"
+import Samhara from "./logo/samhara.png"
+import aryan from "./logo/aryans.jpg"
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      styl:false
+    }
+  }
+
+  componentDidMount() {
+
+    setTimeout(() => this.setState({styl:true}),3000)
+  }
+
+  render() {
+    return (
+        <>
+
+          <img src={aryan} className="aryan"  alt=""/>
+          <div className="App" style={ this.state.styl ? {opacity:"1"}:null}>
+          <div className="mainBox">
+            <div className="title">
+              Layatharang
+            </div>
+            <div className="scoreBox">
+              <div className="scoreHouse" id ="demo" style={{backgroundColor: "#6a1b9a"}}>
+                <div className="logoOut">
+                  <img src={Samhara} className="logo" alt=""/>
+                </div>
+                <div className="point">
+                  31
+                </div>
+              </div>
+              <div className="scoreHouse" style={{backgroundColor: "#1976d2"}}>
+                <div className="logoOut">
+                  <img src={Rakshasa} className="logo" alt=""/>
+                </div>
+                <div className="point">
+                  31
+                </div>
+              </div>
+              <div className="scoreHouse" style={{backgroundColor: "#f57c00"}}>
+                <div className="logoOut">
+                  <img src={Druva} className="logo" alt=""/>
+                </div>
+                <div className="point">
+                  31
+                </div>
+              </div>
+              <div className="scoreHouse" style={{backgroundColor: "#d32f2f"}}>
+                <div className="logoOut">
+                  <img src={Thandava} className="logo" alt=""/>
+                </div>
+                <div className="point">
+                  31
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+          </>
+    );
+  }
 }
 
 export default App;
